022e8d1a8e2eab143073ab0c7c068dccb83749fd
pasim-io <andrewjpasimio@gmail.com>
2026-02-26 12:31:01 -0800

    pid (does nothing lol)

---
+++ inverse_pendulum_pid.py
@@ -0,0 +1,11 @@
+import math
+from simple_pid import PID
+
+class InversePendulumPID():
+    def __init__(self):
+        theta = 0
+        velocity = 0
+        acceleration = 0
+        pid = PID(1,0.1, 0.05, setpoint=1)
+
+    def control_loop(self):
